85fbee7172b9fc70833192cd3dd6ec4e9d9dd33a
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-09-07 15:53:25 +0530

    added user rest api

---
+++ backend/src/main/java/com/chat/application/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.chat.application.controller;
+
+import com.chat.application.entity.User;
+import com.chat.application.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
